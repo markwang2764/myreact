438afe0b9e49304590e2da0b77ef22836042eabe
markwang2764 <276410287@qq.com>
2024-10-18 10:25:31 +0800

    s

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,4 @@
+import { Props, Key } from "shared/ReactTypes";
+export class FiberNode {
+  constructor(tag, pendingProps: Props, key: Key) {}
+}
---
+++ packages/react-reconciler/src/workTags.ts
@@ -0,0 +1,1 @@
+export const functionComponent = 0;
