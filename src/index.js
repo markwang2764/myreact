9f4d30d7c34ce3131eff2210b21cd47b4656ff2f
markwang2764 <276410287@qq.com>
2025-01-16 09:30:09 +0800

    s

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import React from "react";
+const element = <h1 title="foo">Hello, world!</h1>;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "index.js",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React",
+      template: path.join(__dirname, "src/index.html"),
+    }),
+  ],
+};
